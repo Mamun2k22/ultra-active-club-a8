9b127b459cd153b37b54d6f5ef89159bad4e5d38
Mamun2k22 <mamun180037@gmail.com>
2022-09-30 01:08:56 +0600

    1st commit

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import Cart from '../Cart/Cart';
+import Gym from '../Gym/Gym';
+import Nav from '../Nav/Nav';
+
+const Home = () => {
+
+    const [cart, setCart] = useState([]);
+
+    const addToList = (gym) => {
+
+        const newCart = [...cart, gym];
+        setCart(newCart);
+    }
+
+
+    return (
+        <div >
+
+            <div>
+
+            </div>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                <div className='col-span-1 lg:col-span-2'>
+                    <div>
+                        <Nav></Nav>
+                    </div>
+                    <div>
+                        <Gym
+                            addToList={addToList}
+
+                        ></Gym>
+                    </div>
+                </div>
+
+
+                <div className=''>
+                    <Cart
+                        cart={cart}
+                    ></Cart>
+
+                </div>
+            </div>
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+    );
+};
+
+export default Home;
